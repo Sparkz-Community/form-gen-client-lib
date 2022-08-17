4e3bf55d7becce9b5da900b392251001904d640e
jaydenthomas <jayden@ionrev.com>
2022-08-17 14:08:02 -0600

    Install Vue-DatePicker; remove modules; fix stores

--- src/packages/index.js
+++ src/packages/index.js
@@ -1,11 +1,15 @@
 import VueCtkDateTimePicker from 'vue-ctk-date-time-picker';
 import 'vue-ctk-date-time-picker/dist/vue-ctk-date-time-picker.css';
 
+import DatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+
 import VueNumberInput from '@chenfengyuan/vue-number-input';
 
 import VueTelInput from 'vue-tel-input';
 
 export {
+  DatePicker,
   VueCtkDateTimePicker,
   VueNumberInput,
   VueTelInput,
